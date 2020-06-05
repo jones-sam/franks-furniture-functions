a0c89060a2a4b06c4d0f85b235ef3913304d0c82
jones-sam <samjones6058@gmail.com>
2020-06-05 13:01:19 -0300

    tmp commit

--- functions/index.js
+++ functions/index.js
@@ -11,8 +11,8 @@ app.use(express.json())
 const cors = require("cors")
 app.use(cors())
 
-import Stripe from "stripe"
-const stripe = new Stripe(process.env.STRIPE_SK)
+// import Stripe from "stripe"
+// const stripe = new Stripe(process.env.STRIPE_SK)
 // For some reason I don't get intellisense when importing using "require"
 const stripe = require("stripe")(process.env.STRIPE_SK)
 
@@ -23,18 +23,23 @@ const calculatePrice = (items) => {
   items.forEach((item) => {
     db.doc(`/items/${item.itemId}`)
       .get()
-      .then((data) => {})
+      .then((doc) => {
+        console.log(doc)
+        totalCost += doc.data().price * item.quantity
+      })
   })
-  return 12345
+  return totalCost
 }
 
 app.post("/create-payment-intent", async (req, res) => {
   const { items } = req.body
   try {
+    console.log(`calculated price: ${calculatePrice(items)}`)
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: calculatePrice(items),
+      amount: 1234,
       currency: "cad",
     })
+    console.log(`payment intent: ${paymentIntent}`)
     res.send({
       clientSecret: paymentIntent.client_secret,
     })
