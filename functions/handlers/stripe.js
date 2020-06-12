bdafc36168e0d4f4eb949c6818857b1b8053a616
jones-sam <samjones6058@gmail.com>
2020-06-12 12:41:58 -0300

    refractoring

---
+++ functions/handlers/stripe.js
@@ -0,0 +1,39 @@
+// import Stripe from "stripe"
+// const stripe = new Stripe(process.env.STRIPE_SK)
+// For some reason I don't get intellisense when importing using "require"
+const stripe = require("stripe")(process.env.STRIPE_SK)
+
+const { db } = require("../util/admin")
+
+exports.createPaymentIntent = async (req, res) => {
+  const { items } = req.body
+
+  let itemIds = items.map((item) => db.doc(`items/${item.itemId}`))
+  let totalCostCents = 0
+  db.getAll(...itemIds)
+    .then((docs) => {
+      docs.forEach((doc, index) => {
+        totalCostCents += doc.data().price * 100 * items[index].quantity
+      })
+    })
+    .then(() => {
+      stripe.paymentIntents
+        .create({
+          amount: totalCostCents,
+          currency: "cad",
+        })
+        .then((data) => {
+          res.send({
+            clientSecret: data.client_secret,
+          })
+        })
+        .catch((err) => {
+          console.error(err)
+          res.status(400).send({ error: err })
+        })
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).send({ error: "Something went wrong" })
+    })
+}
--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,9 @@ const functions = require("firebase-functions")
 require("dotenv").config()
 const express = require("express")
 const app = express()
-const { db } = require("./util/admin")
 const { resolve } = require("path")
+
+const { createPaymentIntent } = require("./handlers/stripe")
 
 app.use(express.static("."))
 app.use(express.json())
@@ -11,42 +12,7 @@ app.use(express.json())
 const cors = require("cors")
 app.use(cors())
 
-// import Stripe from "stripe"
-// const stripe = new Stripe(process.env.STRIPE_SK)
-// For some reason I don't get intellisense when importing using "require"
-const stripe = require("stripe")(process.env.STRIPE_SK)
-
-app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body
-
-  let itemIds = items.map((item) => db.doc(`items/${item.itemId}`))
-  let totalCostCents = 0
-  db.getAll(...itemIds)
-    .then((docs) => {
-      docs.forEach((doc, index) => {
-        totalCostCents += doc.data().price * 100 * items[index].quantity
-      })
-    })
-    .then(() => {
-      stripe.paymentIntents
-        .create({
-          amount: totalCostCents,
-          currency: "cad",
-        })
-        .then((data) => {
-          res.send({
-            clientSecret: data.client_secret,
-          })
-        })
-        .catch((err) => {
-          console.error(err)
-          res.status(400).send({ error: err })
-        })
-    })
-    .catch((err) => {
-      console.error(err)
-      res.status(500).send({ error: "Something went wrong" })
-    })
-})
+// Stripe
+app.post("/create-payment-intent", createPaymentIntent)
 
 exports.api = functions.https.onRequest(app)
