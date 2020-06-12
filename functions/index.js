9ca4f623df3ac50ba1aeddf16cc4fd64b1351c9b
jones-sam <samjones6058@gmail.com>
2020-06-12 14:28:33 -0300

    algolia index automatically updates on item write in firestore

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions")
 const express = require("express")
 const app = express()
 const { resolve } = require("path")
+const algoliasearch = require("algoliasearch")
 
 const { createPaymentIntent } = require("./handlers/stripe")
 
@@ -15,3 +16,45 @@ app.use(cors())
 app.post("/create-payment-intent", createPaymentIntent)
 
 exports.api = functions.https.onRequest(app)
+
+const client = algoliasearch(
+  functions.config().algolia.appid,
+  functions.config().algolia.apikey
+)
+const itemsIndex = client.initIndex(`items`)
+
+exports.algoliaItemsSync = functions.firestore
+  .document(`items/{doc}`)
+  .onWrite(async (change, _context) => {
+    const oldData = change.before
+    const newData = change.after
+    const data = newData.data()
+    const objectID = newData.id // <-- prop name is important
+
+    if (!oldData.exists && newData.exists) {
+      // creating
+      return itemsIndex.addObject(
+        Object.assign(
+          {},
+          {
+            objectID,
+          },
+          data
+        )
+      )
+    } else if (!newData.exists && oldData.exists) {
+      // deleting
+      return itemsIndex.deleteObject(objectID)
+    } else {
+      // updating
+      return itemsIndex.saveObject(
+        Object.assign(
+          {},
+          {
+            objectID,
+          },
+          data
+        )
+      )
+    }
+  })
